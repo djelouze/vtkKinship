e4e0718e1d422f0b75584718224f464495ecbfac
Jérôme Velut <jerome.velut@gmail.com>
2010-03-30 22:49:19 +0200

    [ENH] Implement the deformation. Not working yet

--- Filters/vtkDeformableMesh.cxx
+++ Filters/vtkDeformableMesh.cxx
@@ -16,9 +16,9 @@ vtkDeformableMesh::vtkDeformableMesh()
    this->NumberOfIterations = 0;
    this->CurrentIteration = 0;
 
-   this->CachedInput = vtkSmartPointer<vtkPolyData>::New( );
-   this->WarpFilter = vtkSmartPointer<vtkWarpVector>::New( );
-   this->ProbeFilter = vtkSmartPointer<vtkProbeFilter>::New( );
+   this->CachedInput = vtkPolyData::New( );
+   this->WarpFilter = vtkWarpVector::New( );
+   this->ProbeFilter = vtkProbeFilter::New( );
 }
 
 
@@ -29,6 +29,15 @@ void vtkDeformableMesh::PrintSelf(ostream& os, vtkIndent indent)
 
 }
 
+//---------------------------------------------------------------------------
+int vtkDeformableMesh::FillInputPortInformation(int port, vtkInformation *info)
+{
+  if( port == 0 ) // input mesh port
+     info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkPolyData");
+  else if( port == 1 ) // image port
+     info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkImageData");
+  return 1;
+}
 
 int vtkDeformableMesh::RequestData(
   vtkInformation *vtkNotUsed(request),
@@ -36,23 +45,47 @@ int vtkDeformableMesh::RequestData(
   vtkInformationVector *outputVector)
 {
    // get the info objects
-   vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
-   vtkInformation *outInfo = outputVector->GetInformationObject(0);
+   vtkInformation *inMeshInfo = inputVector[0]->GetInformationObject(0);
+   vtkInformation *inImageInfo = inputVector[1]->GetInformationObject(0);
+
+   vtkInformation *outMeshInfo = outputVector->GetInformationObject(0);
+
+   vtkPolyData* inputMesh = vtkPolyData::SafeDownCast(
+    inMeshInfo->Get(vtkDataObject::DATA_OBJECT()));
+   vtkImageData* inputImage = vtkImageData::SafeDownCast(
+    inImageInfo->Get(vtkDataObject::DATA_OBJECT()));
+   vtkPolyData* outputMesh = vtkPolyData::SafeDownCast(
+    outMeshInfo->Get(vtkDataObject::DATA_OBJECT()));
+
+   vtkImageData* cachedImage = vtkImageData::New( );
+   cachedImage->ShallowCopy( inputImage );
 
    if( this->IterateFromZero == 1 || this->CurrentIteration == 0 )
    {
       // Copy input
-      // this->CachedInput->DeepCopy( 
+      this->CachedInput->DeepCopy( inputMesh );
       // Reset current iteration
       this->CurrentIteration = 0;
    }
-      
+
+   this->ProbeFilter->SetInput( this->CachedInput );
+   this->ProbeFilter->SetSource( cachedImage );
+
+   this->WarpFilter->SetInputConnection( this->ProbeFilter->GetOutputPort( ) );
+
    while( this->CurrentIteration < this->NumberOfIterations )
    {
+      this->WarpFilter->SetScaleFactor( 200 );
+      this->WarpFilter->Update( );
       
-
-      
+      this->CachedInput->DeepCopy( static_cast<vtkPolyData*>(this->WarpFilter
+                                                             ->GetOutput( )) );
+      this->ProbeFilter->Modified( );
+      this->CurrentIteration ++;      
    }    
   
+   outputMesh->DeepCopy( static_cast<vtkPolyData*>(this->WarpFilter
+                                                       ->GetOutput( )) );
 
+   return( 1 );
 }
