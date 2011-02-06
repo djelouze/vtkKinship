e16f5e304c05d458ce5691cf19489d30aef24dd2
Jérôme Velut <jerome.velut@gmail.com>
2011-02-06 23:02:27 +0100

    [ENH] Add a new class that reslice a volume along a spline (3D->2D)

---
+++ Filters/vtkSplineDrivenImageReslice.cxx
@@ -0,0 +1,236 @@
+#include "vtkSplineDrivenImageReslice.h"
+
+#include"vtkPoints.h"
+#include"vtkPolyData.h"
+#include"vtkCellArray.h"
+#include "vtkImageReslice.h"
+
+#include "vtkFrenetSerretFrame.h"
+#include "vtkPlaneSource.h"
+#include "vtkImageData.h"
+#include "vtkMatrix4x4.h"
+#include "vtkImageAppend.h"
+#include "vtkDoubleArray.h"
+#include "vtkPointData.h"
+#include "vtkMath.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkObjectFactory.h"
+#include "vtkSmartPointer.h"
+#include "vtkStreamingDemandDrivenPipeline.h"
+
+
+vtkCxxRevisionMacro(vtkSplineDrivenImageReslice, "$Revision: 1.31 $");
+vtkStandardNewMacro(vtkSplineDrivenImageReslice);
+
+vtkSplineDrivenImageReslice::vtkSplineDrivenImageReslice( )
+{
+   this->localFrenetFrames = vtkFrenetSerretFrame::New( );
+   this->reslicer = vtkImageReslice::New();
+   this->SliceSize = 15;
+   this->SliceSpacing = 1;
+   this->OffsetPoint = 0;
+   
+   this->SetNumberOfInputPorts( 2 );
+}
+
+vtkSplineDrivenImageReslice::~vtkSplineDrivenImageReslice( )
+{
+	this->localFrenetFrames->Delete( );
+}
+
+//----------------------------------------------------------------------------
+// Specify a source object at a specified table location.
+void vtkSplineDrivenImageReslice::SetPathConnection(int id, vtkAlgorithmOutput* algOutput)
+{
+  if (id < 0)
+    {
+    vtkErrorMacro("Bad index " << id << " for source.");
+    return;
+    }
+
+  int numConnections = this->GetNumberOfInputConnections(1);
+  if (id < numConnections)
+    {
+    this->SetNthInputConnection(1, id, algOutput);
+    }
+  else if (id == numConnections && algOutput)
+    {
+    this->AddInputConnection(1, algOutput);
+    }
+  else if (algOutput)
+    {
+    vtkWarningMacro("The source id provided is larger than the maximum "
+                    "source id, using " << numConnections << " instead.");
+    this->AddInputConnection(1, algOutput);
+    }
+}
+
+//---------------------------------------------------------------------------
+int vtkSplineDrivenImageReslice::FillInputPortInformation(int port, vtkInformation *info)
+{
+  if( port == 0 )
+     info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkImageData");
+   else
+     info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkPolyData");
+
+  return 1;
+}
+
+
+int vtkSplineDrivenImageReslice::RequestInformation (
+  vtkInformation * vtkNotUsed(request),
+  vtkInformationVector** inputVector,
+  vtkInformationVector *outputVector)
+{
+
+  vtkDebugMacro( <<"RequestInformation"<<endl);
+  // get the info objects
+  vtkInformation* outInfo = outputVector->GetInformationObject(0);
+
+  int extent[6] = {0, this->SliceSize / this->SliceSpacing - 1,
+                   0, this->SliceSize / this->SliceSpacing - 1, 
+                   0, 0};
+  double spacing[3] = {this->SliceSpacing,this->SliceSpacing,0};
+
+
+  outInfo->Set(vtkDataObject::SPACING(), spacing, 3);
+  outInfo->Set(vtkStreamingDemandDrivenPipeline::WHOLE_EXTENT(),
+               extent,6);
+
+  return 1;
+}
+
+//! RequestData is called by the pipeline process. 
+int vtkSplineDrivenImageReslice::RequestData(
+  vtkInformation *vtkNotUsed(request),
+  vtkInformationVector **inputVector,
+  vtkInformationVector *outputVector)
+{
+  vtkDebugMacro( <<"RequestData"<<endl);
+  // get the info objects
+  vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
+  vtkInformation *pathInfo = inputVector[1]->GetInformationObject(0);
+  vtkInformation *outInfo = outputVector->GetInformationObject(0);
+
+  // get the input and ouptut
+  vtkImageData *input = vtkImageData::SafeDownCast(
+    inInfo->Get(vtkDataObject::DATA_OBJECT()));
+  vtkImageData *inputCopy = vtkImageData::New( );
+  inputCopy->ShallowCopy( input );
+  vtkPolyData *inputPath = vtkPolyData::SafeDownCast(
+    pathInfo->Get(vtkDataObject::DATA_OBJECT()));
+  vtkPolyData *pathCopy = vtkPolyData::New( );
+  pathCopy->ShallowCopy( inputPath );
+   vtkImageData *output = vtkImageData::SafeDownCast(
+   outInfo->Get(vtkDataObject::DATA_OBJECT()));
+
+   // Compute the local normal and tangent to the path
+   this->localFrenetFrames->SetInput( pathCopy );
+   this->localFrenetFrames->SetInputConnection( this->GetPathConnection( ) );
+   this->localFrenetFrames->SetViewUp( this->Incidence );
+   this->localFrenetFrames->Update( );
+
+   // path will contain PointData array "Tangents" and "Vectors"
+   vtkPolyData* path = static_cast<vtkPolyData*>
+                      (this->localFrenetFrames->GetOutputDataObject( 0 ));
+   // Count how many points are used in the cells
+   // In case of loop, points may be used several times
+   // (note: not using NumberOfPoints because we want only LINES points...)
+   vtkCellArray* lines = path->GetLines( );
+   lines->InitTraversal( );
+   int numberOfPoints = 0;
+   for( int cellIdx = 0; cellIdx < lines->GetNumberOfCells( ); cellIdx++ )
+   {
+      vtkIdType nbCellPoints;
+      vtkIdType* points;
+      lines->GetNextCell( nbCellPoints, points);
+      numberOfPoints += nbCellPoints;
+   }
+
+      vtkIdType nbCellPoints;
+      vtkIdType* points;
+
+      lines->GetNextCell( nbCellPoints, points);
+
+         int ptId = this->OffsetPoint;
+         // Build a new reslicer with image input as input too.
+         this->reslicer->SetInput( inputCopy ); 
+
+         // Get the Frenet-Serret chart at point ptId:
+         // - position (center)
+         // - tangent T
+         // - normal N
+         double center[3];
+         path->GetPoints( )->GetPoint( ptId, center );
+         vtkDoubleArray* pathTangents = static_cast<vtkDoubleArray*>
+                                  (path->GetPointData( )->GetArray( "Tangents" ));
+         double tangent[3];
+         pathTangents->GetTuple( ptId, tangent );
+ 
+         vtkDoubleArray* pathNormals = static_cast<vtkDoubleArray*>
+                                  (path->GetPointData( )->GetArray( "Normals" ));
+         double normal[3];
+         pathNormals->GetTuple( ptId, normal );
+
+         // the Frenet-Serret chart is made of T, N and B = T ^ N
+         double crossProduct[3];
+         vtkMath::Cross( tangent, normal, crossProduct );
+
+         // Build the transformation matrix (inspired from vtkImagePlaneWidget)
+         vtkMatrix4x4* resliceAxes = vtkMatrix4x4::New( );
+         resliceAxes->Identity();
+         double origin[4];
+         // According to vtkImageReslice API:
+         // - 1st column contains the resliced image x-axis
+         // - 2nd column contains the resliced image y-axis
+         // - 3rd column contains the normal of the resliced image plane
+         // -> 1st column is normal to the path
+         // -> 3nd column is tangent to the path
+         // -> 2nd column is B = T^N
+         for ( int comp = 0; comp < 3; comp++ )
+         {
+            resliceAxes->SetElement(0,comp,normal[comp]);
+            resliceAxes->SetElement(1,comp,crossProduct[comp]);
+            resliceAxes->SetElement(2,comp,tangent[comp]);
+
+            origin[comp] = center[comp] - normal[comp]*this->SliceSize/2.
+                               - crossProduct[comp]*this->SliceSize/2.;
+         }
+
+         //! Transform the origin in the homogeneous coordinate space. 
+         //! \todo See why !
+         origin[3] = 1.0;
+         double originXYZW[4];
+         resliceAxes->MultiplyPoint(origin, originXYZW);
+
+         //! Get the new origin from the transposed matrix. 
+         //! \todo See why !
+         resliceAxes->Transpose();
+         double neworiginXYZW[4];
+         resliceAxes->MultiplyPoint(originXYZW, neworiginXYZW);
+
+         resliceAxes->SetElement(0,3,neworiginXYZW[0]);
+         resliceAxes->SetElement(1,3,neworiginXYZW[1]);
+         resliceAxes->SetElement(2,3,neworiginXYZW[2]);
+ 
+         this->reslicer->SetResliceAxes( resliceAxes );
+         this->reslicer->SetInformationInput( input );
+         this->reslicer->SetInterpolationModeToCubic( );
+
+         this->reslicer->SetOutputDimensionality( 2 );
+         this->reslicer->SetOutputOrigin(0,0,0);
+         this->reslicer->SetOutputExtent( 0, this->SliceSize / this->SliceSpacing - 1, 
+                                              0, this->SliceSize / this->SliceSpacing - 1, 
+                                              0, 0);
+         this->reslicer->SetOutputSpacing(this->SliceSpacing, this->SliceSpacing, 0);
+         this->reslicer->Update( );
+
+         resliceAxes->Delete( );
+   output->DeepCopy( this->reslicer->GetOutputDataObject( 0 ) );
+   output->GetPointData( )->GetScalars( )->SetName( "ReslicedImage" );
+
+   return 1;
+
+}
+
---
+++ Filters/vtkSplineDrivenImageReslice.h
@@ -0,0 +1,79 @@
+//! \class vtkSplineDrivenImageReslice
+//! \brief Reslicing of a volume along a path
+//!
+//! Straightened Curved Planar Reformation (Stretched-CPR) builds a 2D image 
+//! from an input path and an input volume. Each point of the path is 
+//! considered as the center of a 2D vtkImageReslicer. Reslicers axes are set
+//! orthogonal to the path. Reslicers output are appended on the z axis. Thus
+//! the output of this filter is a volume with central XZ- and YZ-slices 
+//! corresponding to the Straightened-CPR.
+//!
+//! Input: vtkImageData (InputConnection) and vtkPolyData (PathConnection) 
+//! one polyline representing the path. Typically, the output of vtkSpline can
+//! be used as path input.
+//!
+//! \see Kanitsar et al. "CPR - Curved Planar Reformation", Proc. IEEE  Visualization, 2002, 37-44
+//! \author Jerome Velut
+//! \date 6 february 2011
+
+#ifndef __vtkSplineDrivenImageReslice_h__
+#define __vtkSplineDrivenImageReslice_h__
+
+#include"vtkImageAlgorithm.h"
+
+class vtkFrenetSerretFrame;
+class vtkImageReslice;
+
+class VTK_EXPORT vtkSplineDrivenImageReslice : public vtkImageAlgorithm
+{
+public:
+   vtkTypeRevisionMacro(vtkSplineDrivenImageReslice,vtkImageAlgorithm);
+   static vtkSplineDrivenImageReslice* New();
+
+  //! Specify the path represented by a vtkPolyData wich contains PolyLines
+  void SetPathConnection(int id, vtkAlgorithmOutput* algOutput);
+  void SetPathConnection(vtkAlgorithmOutput* algOutput)
+    {
+      this->SetPathConnection(0, algOutput);
+    };
+  vtkAlgorithmOutput* GetPathConnection( )
+                                 {return( this->GetInputConnection( 1, 0 ) );};
+
+   vtkSetMacro( SliceSize, double );
+   vtkGetMacro( SliceSize, double );
+
+   vtkSetMacro( SliceSpacing, double );
+   vtkGetMacro( SliceSpacing, double );
+
+   vtkSetMacro( OffsetPoint, vtkIdType );
+   vtkGetMacro( OffsetPoint, vtkIdType );
+
+   vtkSetMacro( Incidence, double );
+   vtkGetMacro( Incidence, double );
+
+
+protected:
+   vtkSplineDrivenImageReslice();
+   ~vtkSplineDrivenImageReslice();
+
+   virtual int RequestData(vtkInformation *, vtkInformationVector **, 
+	                                         vtkInformationVector *);
+
+   virtual int FillInputPortInformation(int port, vtkInformation *info);
+   virtual int RequestInformation(vtkInformation*, vtkInformationVector**, 
+	                                                vtkInformationVector*);
+private:
+   vtkSplineDrivenImageReslice(const vtkSplineDrivenImageReslice&);  // Not implemented.
+   void operator=(const vtkSplineDrivenImageReslice&);  // Not implemented.
+
+   vtkFrenetSerretFrame* localFrenetFrames; //!< computes local tangent along path input
+   vtkImageReslice* reslicer; //!< Reslicers array
+
+   vtkIdType SliceSize; //!< Number of pixels around the center points
+   double SliceSpacing; //!< Pixel size
+   double Incidence; //!< Rotation of the initial normal vector.
+   
+   vtkIdType OffsetPoint; //!< Id of the point where the reslicer proceed
+};
+
+#endif //__vtkSplineDrivenImageReslice_h__
