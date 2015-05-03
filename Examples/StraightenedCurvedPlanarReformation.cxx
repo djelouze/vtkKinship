d39c0c107f6120a5e7743b9cd84ee8b35cdcbb0a
Djelouze <jerome.velut@gmail.com>
2015-05-03 21:43:29 +0200

    Use of yaap for straightened curved planar reformation example

--- Examples/StraightenedCurvedPlanarReformation.cxx
+++ Examples/StraightenedCurvedPlanarReformation.cxx
@@ -14,8 +14,28 @@
 #include "vtkXMLImageDataReader.h"
 #include "vtkXMLImageDataWriter.h"
 
+#include "yaap.h"
+
 int main( int argc, char** argv )
 {
+  yaap::Parser parser(argc, argv,"Use of spline-driven image slicer for panoramic imaging");
+  yaap::OptionArg<std::string>* inputImage = parser.AddOptionArg<std::string>('i', "Input image (vti) to reformat along the input path", 1, true);
+  yaap::OptionArg<std::string>* inputPath = parser.AddOptionArg<std::string>('p', "Input path (vtp) along which the input image will be reformatted", 1, true);
+  yaap::OptionArg<std::string>* outputImage = parser.AddOptionArg<std::string>('o', "Output reformatted image (vti)", 1, true);
+
+  yaap::OptionArg<int>* extentOpt = parser.AddOptionArg<int>('e', "Extent of the ouput slices (default to 256x256)", 2, false);
+  yaap::OptionArg<double>* spacingOpt = parser.AddOptionArg<double>('s', "X/Y spacing (default to 1)", 2, false);
+  yaap::OptionArg<double>* thicknessOpt = parser.AddOptionArg<double>('t', "Thickness (Z-spacing - default to 1)", 1, false);
+  
+yaap::Option* helpOpt = parser.AddOption('h', "Show this message");
+
+  if( !parser.IsCommandLineValid( ) || helpOpt->Exists() || argc ==  1)
+  {
+    parser.Usage( );
+    return( 0 );
+  }
+
+
    vtkSmartPointer<vtkXMLImageDataReader> imgReader;
    imgReader = vtkSmartPointer<vtkXMLImageDataReader>::New();
 
@@ -25,15 +45,39 @@ int main( int argc, char** argv )
    vtkSmartPointer<vtkSplineDrivenImageSlicer> reslicer;
    reslicer = vtkSmartPointer<vtkSplineDrivenImageSlicer>::New();
    
+   vtkIdType extent[2] = {255,255};
+   if( extentOpt->Exists() )
+   {
+     extent[0] = extentOpt->GetArgument(0);
+     extent[1] = extentOpt->GetArgument(1);
+   }
+      
+   double thickness = 1.0;
+   if( thicknessOpt->Exists() )
+   {
+     thickness = thicknessOpt->GetValue();
+   }
+
+   double spacing[2] = {1,1};
+   if( spacingOpt->Exists() )
+   {
+     spacing[0] = spacingOpt->GetArgument(0);
+     spacing[1] = spacingOpt->GetArgument(1);
+   }
+ 
+   reslicer->SetSliceSpacing( spacing );
+   reslicer->SetSliceThickness( thickness );
+   reslicer->SetSliceExtent( extent );   
+
    vtkSmartPointer<vtkImageAppend> append;
    append = vtkSmartPointer<vtkImageAppend>::New();
    
    vtkSmartPointer<vtkXMLImageDataWriter> imgWriter;
    imgWriter = vtkSmartPointer<vtkXMLImageDataWriter>::New();
    
-   imgReader->SetFileName(argv[1]);
+   imgReader->SetFileName(inputImage->GetValue().c_str());
    imgReader->Update();
-   pathReader->SetFileName(argv[2]);
+   pathReader->SetFileName(inputPath->GetValue().c_str());
    pathReader->Update();
    
    reslicer->SetInputConnection(imgReader->GetOutputPort() );
@@ -55,7 +99,7 @@ int main( int argc, char** argv )
    append->SetAppendAxis(2);
    append->Update( );
    
-   imgWriter->SetFileName(argv[3]);
+   imgWriter->SetFileName(outputImage->GetValue().c_str());
    imgWriter->SetInputConnection(append->GetOutputPort());
    imgWriter->Write( );
    
